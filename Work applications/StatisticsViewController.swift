7ba1340a39523ba7e3b43256dc0d7006e6c1950c
roudYStrom <anzhelikaroud@yandex.ru>
2025-04-16 22:17:01 +0300

    final

---
+++ Work applications/StatisticsViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class StatisticsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        
+    }
+}
+
