2bbbeb8a2c11cc9c1c57c5f82d1262d1b7ea9d9e
roudYStrom <anzhelikaroud@yandex.ru>
2025-04-19 12:52:31 +0300

    start

--- Work applications/EmojiColorSelectionView.swift
+++ Work applications/EmojiColorSelectionView.swift
@@ -25,4 +25,5 @@ final class NewIrregularEventController: UIViewController,UICollectionViewDataSo
     private let colors: [UIColor] = [
         .colorSelection1, .colorSelection2, .colorSelection3, .colorSelection4, .colorSelection5, .colorSelection6, .colorSelection7, .colorSelection8, .colorSelection9, .colorSelection10, .colorSelection11, .colorSelection12, .colorSelection13, .colorSelection14, .colorSelection15, .colorSelection16, .colorSelection17, .colorSelection18
     ]
+    
 }
