0a6d0346cbb356062576bc6ec34a02841e6a12c2
roudYStrom <anzhelikaroud@yandex.ru>
2025-05-04 22:43:10 +0300

    17/4

--- Tracker_app/AppDelegate.swift
+++ Tracker_app/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import CoreData
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         AnalyticsService.activate()
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let container = NSPersistentContainer(name: "Tracker")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as? NSError {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+              //  fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         return container
